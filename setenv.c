395420769217f72bdc7fa8a874570aa3c490285e
Adam Brimer <abrimer@mac.com>
2021-04-15 03:13:17 +0000

    Fixed betty errors in setenv

--- setenv.c
+++ setenv.c
@@ -37,22 +37,21 @@ int _strncmp(char *s1, char *s2, unsigned int n)
 
 /**
  * _unsetenv - removes env var
- * @name: name of the env var to remove
+ * @n: name of the env var to remove
  *
  * Return: 0 for success, -1 for fail
 */
-int _unsetenv(const char *name)
+int _unsetenv(const char *n)
 {
-	extern char **environ;
 	char **ep, **sp;
 	size_t len;
 
-	if (name == NULL || name[0] == '\0' || _strchr((char *)name, '=') != 0)
+	if (n == NULL || n[0] == '\0' || _strchr((char *)n, '=') != 0)
 		return (-1);
-	len = _strlen((char *)name);
+	len = _strlen((char *)n);
 	for (ep = environ; *ep != NULL; )
 		/* check if current ep string == name */
-		if (_strncmp(*ep, (char *)name, len) == 0 && (*ep)[len] == '=')
+		if (_strncmp(*ep, (char *)n, len) == 0 && (*ep)[len] == '=')
 		{
 			/* Found entry. Remove and shift all following entries back 1 */
 			for (sp = ep; *sp != NULL; sp++)
@@ -65,32 +64,32 @@ int _unsetenv(const char *name)
 
 /**
  * _setenv - sets a variable in env
- * @name: name of the var to set
- * @value: value to set
+ * @n: name of the var to set
+ * @val: value to set
  * @overwrite: overwrite existing value = 1
  *
  * Return: 0 for success, -1 for fail
 */
-int _setenv(const char *name, const char *value, int overwrite)
+int _setenv(const char *n, const char *val, int overwrite)
 {
 	char *ev;
 	int res = 0, i, ii;
 
-	if (name == NULL || name[0] == '\0' || _strchr((char *)name, '=') != 0 || value == NULL)
+	if (n == NULL || n[0] == '\0' || _strchr((char *)n, '=') != 0 || val == NULL)
 		return (-1);
-	if (_getenv(name) != NULL && overwrite == 0)
+	if (_getenv(n) != NULL && overwrite == 0)
 		return (0);
-	_unsetenv(name);
+	_unsetenv(n);
 	/* malloc for var name + = + value + null byte */
-	ev = malloc(sizeof(char) * (_strlen((char *)name) + _strlen((char *)value) + 2));
+	ev = malloc(sizeof(char) * (_strlen((char *)n) + _strlen((char *)val) + 2));
 	if (ev == NULL)
 		return (-1);
-	for (i = 0, ii = 0; name[i]; i++, ii++)
-		ev[ii] = name[i];
+	for (i = 0, ii = 0; n[i]; i++, ii++)
+		ev[ii] = n[i];
 	ev[ii] = '=';
 	ii++;
-	for (i = 0; value[i]; i++, ii++)
-		ev[ii] = value[i];
+	for (i = 0; val[i]; i++, ii++)
+		ev[ii] = val[i];
 	res = putenv(ev);
 	return ((res != 0) ? -1 : 0);
 }
