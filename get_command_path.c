6772f9dffff9d4abaa1276f0c7f78afe39b26cad
Adam Brimer <abrimer@mac.com>
2021-04-15 23:25:50 +0000

    Handling difference between unset and empty PATH

--- get_command_path.c
+++ get_command_path.c
@@ -69,6 +69,8 @@ char **get_env_paths(void)
 	path = _getenv("PATH");
 	/* fix to use pwd if PATH is null */
 	if (path == NULL)
+		return (NULL);
+	if (_strcmp(path, "") == 0)
 	{
 		pwd = _getenv("PWD");
 		env_paths = malloc(sizeof(char *) * 2);
